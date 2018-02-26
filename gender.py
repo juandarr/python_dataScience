539a49dd313f032c23502d45cee07e0cc649ef1a
Juan David Rios <juandarr@gmail.com>
2018-02-26 00:05:20 -0500

    Project for gender classification given 3 features and using decision trees completed. The weekly challenge is stated for further study

--- gender.py
+++ gender.py
@@ -4,4 +4,22 @@ from sklearn import tree
 
 #[height, weight, shoe size]
 
-X= [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[],[]]
+X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42],[181,85,43]]
+
+Y = ['male','female','female','female','male','male','male','female','male','female','male']
+
+clf = tree.DecisionTreeClassifier()
+
+output = clf.fit(X,Y)
+
+prediction = output.predict([190,70,43])
+
+print(prediction)
+
+#Challenge 
+#1. Use any 3 SciKit-Learn Models on this dataset
+
+#2. Compare results
+
+#3. Print the best one
+
