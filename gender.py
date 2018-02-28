b7c6be49b3d0cc3ef7a51ad84d60224deaa743d4
Juan David Rios <juandarr@gmail.com>
2018-02-27 23:40:26 -0500

    Added support vector machines classifier to define the gender given the output data

--- gender.py
+++ gender.py
@@ -1,18 +1,21 @@
-from sklearn import tree    
-
+from sklearn import tree,svm    
+import numpy as np
 #We are gonna use a decision tree here
 
 #[height, weight, shoe size]
 
-X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42],[181,85,43]]
+X = np.array([[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],[171,75,42],[181,85,43]])
 
 Y = ['male','female','female','female','male','male','male','female','male','female','male']
 
-clf = tree.DecisionTreeClassifier()
+#clf = tree.DecisionTreeClassifier()
+
+clf = svm.SVC()
 
 output = clf.fit(X,Y)
 
-prediction = output.predict([190,70,43])
+
+prediction = output.predict([[190,70,43]])
 
 print(prediction)
 
